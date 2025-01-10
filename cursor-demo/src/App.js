b338aed8bdb6e55914f6c403f9b132182a245ca4
AugustZer0 <qweraz157890@gmail.com>
2025-01-10 16:46:40 +0900

    기능/#7 stat.js 오각형 완성 App.js 하이퍼링크 다시 살림
    stat.js에서 오각형 db값 기반으로 생성됨.
    또한 App.js에서 하이퍼링크 기능 구현함.

--- cursor-demo/src/App.js
+++ cursor-demo/src/App.js
@@ -9,16 +9,35 @@ import Register from "./routes/register";
 import Page1 from "./routes/page1";
 import Login from "./routes/login";
 import Emotion from "./routes/emotion";
+import Stat from "./routes/stat";
+
 function App() {
   return (
     <Router>
       <div>
-        
+      <ul>
+            <li>
+              <Link to="/register">Go to Register</Link>
+            </li>
+            <li>
+              <Link to="/page1">Go to Page1</Link>
+            </li>
+            <li>
+              <Link to="/login">Go to Login</Link>
+            </li>
+            <li>
+              <Link to="/emotion">Go to Emotion</Link>
+            </li>
+            <li>
+              <Link to="/stat">Go to Stat</Link>
+            </li>
+          </ul>
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/page1" element={<Page1 />} />
           <Route path="/login" element={<Login />} />
           <Route path="/emotion" element={<Emotion />} />
+          <Route path="/stat" element={<Stat />} />
         </Routes>
       </div>
     </Router>
